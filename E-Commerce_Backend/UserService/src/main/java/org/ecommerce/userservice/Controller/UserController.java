f648adbf3234fa0f385f646bde3f48114734bc29
oshanLahiru0307 <klahiruoshan@gmail.com>
2026-06-14 10:00:24 +0530

    intial commit message

---
+++ E-Commerce_Backend/UserService/src/main/java/org/ecommerce/userservice/Controller/UserController.java
@@ -0,0 +1,12 @@
+package org.ecommerce.userservice.Controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class UserController {
+
+
+}
